d03f371a6d77c4d6d2d213a8e56d5352e92c3446
Bill Baker <bill.baker@synapsewireless.com>
2018-07-12 09:39:43 -0500

    Documentation and name change to avoid collision.

--- comprehension.py
+++ comprehension.py
@@ -1,4 +1,7 @@
 squares = [x**2 for x in range(100) if x < 10]
+# x**2 - third step - do something to x
+# for x in range(100) - first step - generate x
+# if x < 10 - second step - conditional for ending comprehension when false.
 
 print(squares)
 
